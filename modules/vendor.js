c32ab4e3acc1557e49426f4f26a993a37cdd4920
antoni <antoni.dev0@gmail.com>
2021-11-03 11:03:28 -0700

    completes phase one WIP tests

--- modules/vendor.js
+++ modules/vendor.js
@@ -3,33 +3,27 @@
 const events = require('../utils/event-pool.js')
 const faker = require('faker')
 
-events.on( 'pickup-start', handlePickup  )
-events.on( 'delivered', handleDelivered  )
+events.on( 'in-transit', handleDelivered  )
 
-function handlePickup(){
-  setTimeout(() => {
-    let eventObj = {
-      event: 'pickup-start',
-      time: new Date(),
-      payload: {
-        store: faker.company.companyName(), 
-        orderId: faker.datatype.uuid(),
-        customer: `${faker.name.firstName()}, ${faker.name.lastName()}`,
-        address: `${faker.address.city()}, ${faker.address.state(true)}`
+setTimeout(() => {
+      let eventObj = {
+        event: 'order-ready',
+        time: new Date(),
+        payload: {
+          store: faker.company.companyName(), 
+          orderId: faker.datatype.uuid(),
+          customer: `${faker.name.firstName()}, ${faker.name.lastName()}`,
+          address: `${faker.address.city()}, ${faker.address.state(true)}`
+        }
       }
-    }
-    console.log('EVENT ',eventObj)
-    events.emit('pickup-now', eventObj)
-  },3000)
-}
-
+      events.emit('ready', eventObj)
+},0);
+  
 function handleDelivered(eventObj){
-  setTimeout( () => {
+  setTimeout(()=>{
     console.log(`VENDOR: Thank you for delivering '${eventObj.payload.orderId}'`)
     eventObj.event = 'delivered'
     eventObj.time = new Date()
-    console.log('EVENT',eventObj)
-  }, 2000)
+    events.emit('delivered', eventObj)
+  },0)
 }
-
-module.exports = { handlePickup, handleDelivered }
